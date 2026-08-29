e449633051808dd899b84132248a402929f7f91e
janardhanmajumder <subrotomojumder03@gmail.com>
2026-08-29 22:47:34 +0600

    project initilization with docker setup

---
+++ 02-setup-local-redis/src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import Redis from "ioredis";
+import mongoose from "mongoose";
+
+const app = express();
+
+const redis = new Redis(
+  process.env.REDIS_URL || {
+    host: "localhost",
+    port: 6379,
+  },
+);
+
+app.get("/redis", async (req, res) => {
+  const value = await redis.ping();
+  res.send({ redis: value });
+});
+
+app.get("/mongo", async (req, res) => {
+  const url =
+    process.env.MONGO_URL || "mongodb://localhost:27017/janardhan_redis";
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(url);
+  }
+  res.send({
+    mongo: "connected",
+    database: mongoose.connection.name,
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
